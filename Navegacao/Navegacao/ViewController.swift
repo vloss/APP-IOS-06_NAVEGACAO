4cc49de92f299002887eb1367b97feb0bbf38c35
vloss <viniciusvianaloss@gmail.com>
2023-01-11 23:29:17 -0300

    Continuação de navegação entre telas.

--- Navegacao/Navegacao/ViewController.swift
+++ Navegacao/Navegacao/ViewController.swift
@@ -13,7 +13,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+//    // Toda vez que a tela é tocada.
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        // Executar uma SEGUE
+//        performSegue(withIdentifier: "segue", sender: nil)
+//    }
 
-
+    @IBAction func showSecondScreen(_ sender: Any) {
+        // Executar uma SEGUE
+        performSegue(withIdentifier: "segue", sender: nil)
+    }
+    
 }
 
